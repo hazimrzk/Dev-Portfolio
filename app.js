d5e551b1998e1c733bf90ad2cf5f72507ad8d4e8
Hazim Razak <hazimrazak@Hazims-Macbook.local>
2024-02-20 02:20:16 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+function showOnScrollOnce(entries) {
+    entries.forEach(entry => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}
+
+function showOnScrollRepeated(entries) {
+    entries.forEach(entry => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+}
+
+document.querySelectorAll('a').forEach(function (elem) {
+    elem.setAttribute('target', '_blank');
+    elem.setAttribute('rel', 'noopener');
+})
+
+const observer = new IntersectionObserver(showOnScrollRepeated);
+const hiddenElements = document.querySelectorAll('*');
+hiddenElements.forEach((el) => observer.observe(el))
