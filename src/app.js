b50da452e1e64ccc75c1aefd359025dd1209fcf8
Hazim Razak <hazimrazak@Hazims-Macbook.local>
2025-01-05 18:49:10 +0800

    Fixed JSX tagging and app routing

---
+++ src/app.js
@@ -0,0 +1,22 @@
+// function showOnScrollRepeated(entries) {
+//     entries.forEach(entry => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         }
+//         else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// }
+
+document.querySelectorAll('a').forEach(function (elem) {
+    if (elem.getAttribute("href") != "mailto:azizul.hazim.00@gmail.com") {
+        elem.setAttribute('target', '_blank');
+        elem.setAttribute('rel', 'noopener');
+    }
+})
+
+// const observer = new IntersectionObserver(showOnScrollRepeated);
+// const hiddenElements = document.querySelectorAll('*');
+// hiddenElements.forEach((el) => observer.observe(el))
